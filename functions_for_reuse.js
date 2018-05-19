1debc77933b1a2a24cc31f1b77c93656c07d879f
Steven Brown <sgbrown@mac.com>
2018-05-19 14:50:09 +1200

    remove testing code, add comments

--- functions_for_reuse.js
+++ functions_for_reuse.js
@@ -1,12 +1,3 @@
-function eligible_has_owned_home (has_owned_home) {
-  return !has_owned_home;
+function eligible_has_owned_home (has_owned_home) { // boolean parameter
+  return !has_owned_home; //boolean return value
 }
-
-let has_owned_home = true;
-let result = eligible_has_owned_home(has_owned_home) ? "Eligible" : "Not eligible";
-console.log("Has owned home: -> "+result);
-
-has_owned_home = false;
-result = eligible_has_owned_home(has_owned_home) ? "Eligible" : "Not eligible";
-console.log("Hasn't owned home: -> "+result);
-
