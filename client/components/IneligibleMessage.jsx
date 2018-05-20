162f16ea27e9091ee2fd803f5a883fc93bf9aa4e
YWHo <YWHo@users.noreply.github.com>
2018-05-20 12:01:10 +1200

    replace kiwi saver years to text input question

--- client/components/IneligibleMessage.jsx
+++ client/components/IneligibleMessage.jsx
@@ -4,13 +4,13 @@ export default function IneligibleMessage(props) {
     if (props.isWarning) {
         return (
             <div className="container">
-                <div className="ml-1 mr-1 pl-2 pr-2 text-dark bg-warning">{props.message}</div>
+                <div className="mt-1 ml-1 mr-1 pl-2 pr-2 text-dark bg-warning">{props.message}</div>
             </div>
         )
     } else {
         return (
             <div className="container">
-                <div className="ml-1 mr-1 pl-2 pr-2 container text-white bg-danger">{props.message}</div>
+                <div className="mt-1 ml-1 mr-1 pl-2 pr-2 container text-white bg-danger">{props.message}</div>
             </div>
         )
     }
