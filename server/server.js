e74ea6f38d259099a55867303d3b403c570854f6
JudyPuff <judy.yun.ting@gmail.com>
2018-05-20 13:55:02 +1200

    file changed payability.js

--- server/server.js
+++ server/server.js
@@ -4,15 +4,16 @@ const cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const widgets = require('./routes/widgets')
+const kiwisaver = require('./routes/kiwisaver')
 
 const server = express()
 
 server.use(bodyParser.json())
-server.use(cors({origin: 'http://localhost:8080'}))
+server.use(cors({origin: 'http://192.168.1.70:8080'}))
 server.use(express.static(path.join(__dirname, '../public')))
 
-server.use('/api/v1/widgets', widgets)
+server.use('/api/v1/ks', kiwisaver)
+
 
 module.exports = server
 
