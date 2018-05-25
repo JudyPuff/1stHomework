1ee4268230ff81afb674c0ca322e69137899bfaa
YWHo <YWHo@users.noreply.github.com>
2018-05-25 16:52:03 +1200

    replace HSPayability() with calcHomeStartGrant()

--- client/payability.js
+++ client/payability.js
@@ -3,31 +3,51 @@
 let years_in_ks = []
 
 
-function HSPayability(years_in_ks){
-// switch (years_in_ks) {
-//   case 0:
-//   case 1:
-//   case 2:
-//     return false;
-//   case 3:
-//   case 4:
-//     let HS_grant = years_in_ks * 1000
-//     return HS_grant
-//   default:
+function calcHomeStartGrant(numYearsContributed, isNewHouse) {
+  if (numYearsContributed < 3) {
+    return 0 
+  }
+
+  let baseGrantPerYear = 1000
+  let possibleGrant = numYearsContributed * baseGrantPerYear
+
+  if (possibleGrant > 5000) {
+    possibleGrant = 5000
+  }
+
+  if (isNewHouse) {
+    possibleGrant *= 2
+  }
+
+  return possibleGrant
+
+}
+
+// function HSPayability(years_in_ks){
+// // switch (years_in_ks) {
+// //   case 0:
+// //   case 1:
+// //   case 2:
+// //     return false;
+// //   case 3:
+// //   case 4:
+// //     let HS_grant = years_in_ks * 1000
+// //     return HS_grant
+// //   default:
+// //     let HS_grant = 5000
+// //     return HS_grant
+// // }
+
+// if (years_in_ks < 3) {
+//   return false
+// } else if (years_in_ks >= 5) {
 //     let HS_grant = 5000
 //     return HS_grant
+//  } else {
+//     let HS_grant = years_in_ks * 1000
+//     return HS_grant
+//   }
 // }
-
-if (years_in_ks < 3) {
-  return false
-} else if (years_in_ks >= 5) {
-    let HS_grant = 5000
-    return HS_grant
- } else {
-    let HS_grant = years_in_ks * 1000
-    return HS_grant
-  }
-}
 
 let savings = []
 let gross_ks = []
