0cdd617a42ba9d1914881191d3556a1ccf110686
YWHo <YWHo@users.noreply.github.com>
2018-05-28 13:13:14 +1200

    add colors to header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -5,9 +5,9 @@ export default class Header extends React.Component {
     render() {
         return (
 
-            <div className="jumbotron">
+            <div className="jumbotron text-white bg-info">
                 <h1>KiwiSaver and HomeStart Grant Eligibility calculator</h1>
-                <p>Find out if you are eligible to buy a house with KiwiSaver and HomeStart Grant.</p>
+                <p className="text-light">Find out if you are eligible to buy a house with KiwiSaver and HomeStart Grant.</p>
             </div>
 
         )
